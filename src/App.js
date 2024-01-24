9dd7672c132de90bced9b9b318acd8f1a7fd21ae
Your Name <Your email>
2024-01-24 14:49:56 +0100

    first version docplanner with react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import  Nav  from './Components/Nav.js'
+import Overaly from './Components/Overaly.js'
+
+const App = () => {
+  return (
+    <>
+      <Nav/>
+      <Overaly/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Nav.js
@@ -0,0 +1,48 @@
+import React from "react";
+import "../CSS/Nav.css";
+const Navbar = () => {
+  return ( 
+    <div className="Nav">
+      <img
+        src="https://www.docplanner.com/img/logo-default-group-en.svg?v=1"
+        alt=""
+        srcset=""
+      />
+      <div className="menu">
+        <ul className="drop">
+          <li>
+            <a href="">
+              {" "}
+              <span>About us</span>{" "}
+            </a>
+          </li>
+          <li>
+            <a href="">Career</a>
+          </li>
+          <li className="list">
+            <a href="">Departments</a>
+            <ul className="drop-list">
+              <li>
+                <a href="">Marketing & PR</a>
+              </li>
+              <li>
+                <a href="">Customer Success & Sales</a>{" "}
+              </li>
+              <li>
+                <a href="">IT,Product,Data</a>
+              </li>
+              <li>
+                <a href="">Finance & Administartion</a>
+              </li>
+              <li>
+                <a href="">HR & more</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+  );
+};
+export default Navbar;
